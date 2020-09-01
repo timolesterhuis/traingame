33d8392b735e82775b4b4963a99774f9b433d83d
Timo Lesterhuis <timo.lesterhuis@nl.ey.com>
2020-09-01 22:24:59 +0200

    Bump version: 0.2.2 --> 0.3.0

--- src/traingame/__version__.py
+++ src/traingame/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "traingame"
 __description__ = "Train your own AI and race it!"
 __url__ = ""
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __author__ = "Timo Lesterhuis"
 __author_email__ = "timo.lesterhuis@nl.ey.com"
